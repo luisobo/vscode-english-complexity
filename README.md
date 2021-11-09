4024e2a33b678d4199c5be6c17e6b622786ffaba
Luis Solano Bonet <contact@luissolano.com>
2021-11-09 14:30:54 +0100

    Highlight sentences above a complexity score

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
 # VS Code English Complexity
 
-This VS Code Extension calculates the complexity of your english.
+This VS Code Extension highlights complex sentences in your english writing. It works on plain text and markdown files.
 
-# Decorator Sample
-
-This folder contains a sample VS code extension that demonstrates the editor decorator API.
-
-The sample creates a decoration for each number that appears in the active editor. It
-demonstrates some of the decorator features such as borders, background colors, cursors
-and hovers.
-
-The sample also shows the use of a user defined themeable color. Instead of hardcoding a color value this allows users (and themes) to redefine the color in the user settings.
-
-![sample](preview.png)
-
-## VSCode API
-
-The sample code show the usage of the vscode.[`TextEditor.setDecorations`](https://code.visualstudio.com/api/references/vscode-api#TextEditor.setDecorations) and [`vscode.window.createTextEditorDecorationType`](https://code.visualstudio.com/api/references/vscode-api#window.createTextEditorDecorationType) APIs as well as the `colors` contribution point.
-
-## Running the Sample
+## Running the Extension
 
 * `npm install` to initialize the project
 * `npm run watch` to start the compiler in watch mode
 * open this folder in VS Code and press `F5`
 * this will open the `[Extension Development Host]` window, running the extension:
-  * Open any document that contains single and multi-digit numbers.
-  * The extension will decorate single and multiple-digit numbers as shown in the screenshot above.
-  * In the user settings, add
+  * Open any plain text or markdown files.
+  * The extension will highlight senteces with considered complex.
+  * You can control the complexity threshold in the user settings, add
     ```
-    "workbench.colorCustomizations": {
-        "myextension.largeNumberBackground": "#ff00ff"
-    }
+    "englishcomplexity.complexityThreshold": 50
     ```
-    to customize the large number decoration color.
+    to customize the complexity threshold. The lower the number, the more complexity is allowed. See the table below for reference.
+
+| Score |    Difficulty     |
+|-------|-------------------|
+|90-100 | Very Easy         |
+| 80-89 | Easy              |
+| 70-79 | Fairly Easy       |
+| 60-69 | Standard          |
+| 50-59 | Fairly Difficult  |
+| 30-49 | Difficult         |
+| 0-29  | Very Confusing    |
--- src/extension.ts
+++ src/extension.ts
@@ -1,58 +1,79 @@
 import * as vscode from 'vscode';
+import * as rs from 'text-readability';
+import { split } from 'sentence-splitter';
 
-// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('decorator sample is activated');
+	console.log('vscode-english-complexity is activated');
 
 	let timeout: NodeJS.Timer | undefined = undefined;
 
-	// create a decorator type that we use to decorate small numbers
-	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		borderWidth: '1px',
-		borderStyle: 'solid',
-		overviewRulerColor: 'blue',
-		overviewRulerLane: vscode.OverviewRulerLane.Right,
-		light: {
-			// this color will be used in light color themes
-			borderColor: 'darkblue'
-		},
-		dark: {
-			// this color will be used in dark color themes
-			borderColor: 'lightblue'
-		}
+	const fairlyDifficultDecoration = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		textDecoration: `underline rgb(150,0,0) dotted`,
 	});
 
-	// create a decorator type that we use to decorate large numbers
-	const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
+	const difficultDecoration = vscode.window.createTextEditorDecorationType({
 		cursor: 'crosshair',
-		// use a themable color. See package.json for the declaration and default values.
-		backgroundColor: { id: 'myextension.largeNumberBackground' }
+		textDecoration: `underline rgb(200,0,0) dotted`,
+	});
+
+	const veryConfusingDecoration = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		textDecoration: `underline rgb(250,0,0) dotted`,
 	});
 
 	let activeEditor = vscode.window.activeTextEditor;
+
+	function scoreToWords(score: number): string {
+		if (score < 30) return "Very Confusing";
+		if (score < 50) return "Difficult";
+		if (score < 60) return "Fairly Difficult";
+		if (score < 70) return "Standard";
+		if (score < 80) return "Fairly Easy";
+		if (score < 90) return "Easy";
+		return "Very Easy";
+	}
 
 	function updateDecorations() {
 		if (!activeEditor) {
 			return;
 		}
-		const regEx = /\d+/g;
+
 		const text = activeEditor.document.getText();
-		const smallNumbers: vscode.DecorationOptions[] = [];
-		const largeNumbers: vscode.DecorationOptions[] = [];
-		let match;
-		while ((match = regEx.exec(text))) {
-			const startPos = activeEditor.document.positionAt(match.index);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
-			if (match[0].length < 3) {
-				smallNumbers.push(decoration);
-			} else {
-				largeNumbers.push(decoration);
+		const sentences = split(text);
+
+		const complexityThreshold = vscode.workspace.getConfiguration("englishcomplexity").get<number>('complexityThreshold', 60);
+
+		const veryConfusingSentences: vscode.DecorationOptions[] = [];
+		const difficultSentences: vscode.DecorationOptions[] = [];
+		const fairlyDifficultSentences: vscode.DecorationOptions[] = [];
+		for (const sentence of sentences) {
+			if (sentence.type !== 'Sentence') {
+				continue;
+			}
+
+			const complexityScore = rs.fleschReadingEase(sentence.raw);
+
+			if (complexityScore < complexityThreshold) {
+				const startPos = activeEditor.document.positionAt(sentence.range[0]);
+				const endPos = activeEditor.document.positionAt(sentence.range[1]);
+				const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Complexity: **' + scoreToWords(complexityScore) + ' (' + complexityScore + ')' + '**' };
+
+				if (complexityScore < 30) {
+					veryConfusingSentences.push(decoration);
+				} else if (complexityScore < 50) {
+					difficultSentences.push(decoration);
+				} else {
+					fairlyDifficultSentences.push(decoration);
+				}
+
 			}
 		}
-		activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-		activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
+
+		activeEditor.setDecorations(fairlyDifficultDecoration, fairlyDifficultSentences);
+		activeEditor.setDecorations(difficultDecoration, difficultSentences);
+		activeEditor.setDecorations(veryConfusingDecoration, veryConfusingSentences);
 	}
 
 	function triggerUpdateDecorations() {
